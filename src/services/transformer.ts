ab6b22f86bfc4780a274073538d7be5e9c376718
mbap-dev <moises@mbap.dev>
2025-05-10 11:10:25 -0300

    Refactor phone number extraction logic for clarity.
    Simplify and streamline the `extractDestinyPhone` function by introducing a helper function `getValidPhone`. This improves readability, reduces code duplication, and ensures consistent handling of phone number formatting.

--- src/services/transformer.ts
+++ src/services/transformer.ts
@@ -339,35 +339,23 @@ export const isValidPhoneNumber = (value: string, nine = false): boolean => {
 }
 
 export const extractDestinyPhone = (payload: object) => {
-  const data = payload as any
-  const number = data?.to || (
-    (
-      data.entry
-      && data.entry[0]
-      && data.entry[0].changes
-      && data.entry[0].changes[0]
-      && data.entry[0].changes[0].value
-    ) && (
-      (
-        data.entry[0].changes[0].value.contacts
-        && data.entry[0].changes[0].value.contacts[0]
-        && data.entry[0].changes[0].value.contacts[0].wa_id?.replace('+', '')
-      ) || (
-        data.entry[0].changes[0].value.statuses
-        && data.entry[0].changes[0].value.statuses[0]
-        && data.entry[0].changes[0].value.statuses[0].recipient_id?.replace('+', '')
-      ) || (
-        data.entry[0].changes[0].value.messsages
-        && data.entry[0].changes[0].value.messsages[0]
-        && data.entry[0].changes[0].value.messsages[0].from?.replace('+', '')
-      )
-    )
-  )
+  const data = payload as any;
+
+  const getValidPhone = (phone?: string) =>
+    phone && phone.trim() !== '' ? phone.replace('+', '') : null;
+
+  const number =
+    data?.to ||
+    getValidPhone(data?.entry?.[0]?.changes?.[0]?.value?.contacts?.[0]?.wa_id) ||
+    getValidPhone(data?.entry?.[0]?.changes?.[0]?.value?.statuses?.[0]?.recipient_id) ||
+    getValidPhone(data?.entry?.[0]?.changes?.[0]?.value?.messages?.[0]?.from);
+
   if (!number) {
-    throw Error(`error on get phone number from ${JSON.stringify(payload)}`)
+    throw Error(`error on get phone number from ${JSON.stringify(payload)}`);
   }
-  return number
-}
+
+  return number;
+};
 
 export const getGroupId = (payload: object) => {
   const data = payload as any
